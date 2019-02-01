c017a820f04bc4849a7ae971a64ff65fc58878f0
Adam Tavener <adam.tavener@ft.com>
2019-02-01 17:55:15 +0000

    Popup (#3)
    * make booking form a popup
    * csrp for iframe
    * use babel to parse js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,34 @@
 */
 require('typeface-montserrat');
 require('./main.scss');
+
+/*
+  popup
+*/
+const HIDE_CSS_CLASS = 'd-none';
+const hideEl = (el) => el.classList.add(HIDE_CSS_CLASS);
+const showEl = (el) => el.classList.remove(HIDE_CSS_CLASS);
+
+const popupFunc = (btn, form, mask) => {
+  hideEl(form);
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    showEl(form);
+  });
+  mask.addEventListener('click', (e) => {
+    e.preventDefault();
+    hideEl(form);
+  })
+};
+
+(() => {
+  document.body.classList.add('js');
+  const FORM_BTN_SELECTOR = '.js-book-form';
+  const FORM_SELECTOR = '.js-form';
+  const MASK_SELECTOR = '.mask';
+  popupFunc(
+    document.querySelector(FORM_BTN_SELECTOR),
+    document.querySelector(FORM_SELECTOR),
+    document.querySelector(MASK_SELECTOR)
+  );
+})();
